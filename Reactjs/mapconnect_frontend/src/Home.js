563136c7d52b5bbd1713f569789a211c12c2bbe8
Ayush Sachan <ayush21031@iiitd.ac.in>
2024-06-04 12:33:17 +0530

    Frontend added in home, login, signup

--- Reactjs/mapconnect_frontend/src/Home.js
+++ Reactjs/mapconnect_frontend/src/Home.js
@@ -1,11 +1,30 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-function Home(){
-    return(
-        <div>
-            <h1>Welcome to Home</h1>
-            <button><Link to = "/login">Visit Login</Link></button>
-        </div>
-    );
+// function Home(){
+//     return(
+//         <div>
+//             <h1>Welcome to Home</h1>
+//             <button><Link to = "/login">Visit Login</Link></button>
+//         </div>
+//     );
+// }
+// export default Home;
+
+import { Link } from "react-router-dom";
+import "./Home.css"; // Import CSS file
+
+function Home() {
+  return (
+    <div>
+      <div className="home-content">
+        <h1>Welcome To MapConnect</h1>
+        <p>Project in Development Phase</p>
+        <Link to="/login" className="login-link">
+          <button className="login">Try it ↗️</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
