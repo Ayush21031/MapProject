09b34b0f6f17837d06574b0f1d057a7f0d00450c
Madhur-iiitd <madhur21063@iiitd.ac.in>
2024-06-27 14:57:41 +0900

    scroll function

--- Reactjs/mapconnect_frontend/src/users/UserPage.js
+++ Reactjs/mapconnect_frontend/src/users/UserPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState , useCallback,useRef} from "react";
+import React, { useContext, useEffect, useState, useCallback, useRef } from "react";
 import { UserContext } from "../context/UserContext";
 import { ContactContext } from "../context/ContactContext";
 import { MessageContext } from "../context/MessageContext";
@@ -18,7 +18,7 @@ import io from 'socket.io-client';
 const UserPage = () => {
   const { userData, fetchUserData } = useContext(UserContext);
   const { contacts, fetchContacts } = useContext(ContactContext);
-  const { messages, fetchMessages, setMessages} = useContext(MessageContext);
+  const { messages, fetchMessages, setMessages } = useContext(MessageContext);
   const [msg, setmsg] = useState('');
   const navigate = useNavigate();
   const [showPopup, setShowPopup] = useState(false);
@@ -27,63 +27,80 @@ const UserPage = () => {
   const [socket, setSocket] = useState(null);
   const chatViewRef = useRef(null);
 
-  const scrollToBottom = () => {
+  const scrollToBottom = useCallback(() => {
     if (chatViewRef.current) {
-      chatViewRef.current.scrollTop = chatViewRef.current.scrollHeight;
+      const scrollToBottomIfNeeded = () => {
+        const { scrollTop, scrollHeight, clientHeight } = chatViewRef.current;
+        const maxScrollTop = scrollHeight - clientHeight;
+
+        // Check if we're already at the bottom (within 20px tolerance)
+        if (maxScrollTop - scrollTop > 20) {
+          chatViewRef.current.scrollTop = maxScrollTop;
+          // If we haven't reached the bottom, request another animation frame
+          requestAnimationFrame(scrollToBottomIfNeeded);
+        }
+      };
+
+      // Start the scrolling process
+      requestAnimationFrame(scrollToBottomIfNeeded);
     }
-  };
-  
-  const addMsg = useCallback( async (chat_id, sender, msg, date_time) =>{
+  }, []);
+
+  const addMsg = useCallback(async (chat_id, sender, msg, date_time) => {
     try {
       const res = await fetch('http://localhost:3000/user/addmsg/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ "chat_id":chat_id, "sender":sender, "msg":msg, "date_time":date_time }),
+        body: JSON.stringify({ "chat_id": chat_id, "sender": sender, "msg": msg, "date_time": date_time }),
         credentials: 'include'
       });
 
       const data = await res.json();
-      setMessages([...messages, {chat_id, msg, sender, date_time}]);
+      setMessages([...messages, { chat_id, msg, sender, date_time }]);
       scrollToBottom();
-      socket.emit('send_message', {chat_id, sender, msg, date_time, contact_name: selectedUser.contact_name})
+      socket.emit('send_message', { chat_id, sender, msg, date_time, contact_name: selectedUser.contact_name })
       if (!res.ok) {
         throw new Error(data.detail);
       }
     } catch (error) {
       console.log(error)
     }
-  
-  }) 
+
+  })
 
 
 
 
   useEffect(() => {
-    if(userData && userData.email){
-      const newsocket = io('http://localhost:3000', {query: {email: userData.email}});
+    if (userData && userData.email) {
+      const newsocket = io('http://localhost:3000', { query: { email: userData.email } });
       setSocket(newsocket);
 
-       return () => {
+      return () => {
         newsocket.disconnect();
       }
     }
-  },[userData]);
+  }, [userData]);
 
   useEffect(() => {
-    if(socket){
+    scrollToBottom();
+  }, [messages, scrollToBottom]);
 
-      socket.on('new_message', ({chat_id, msg, sender, date_time}) => {
-        if(selectedUser && selectedUser.chat_id === chat_id){
+  useEffect(() => {
+    if (socket) {
+
+      socket.on('new_message', ({ chat_id, msg, sender, date_time }) => {
+        if (selectedUser && selectedUser.chat_id === chat_id) {
           // fetchMessages(chat_id);
-          setMessages([...messages, {chat_id, msg, sender, date_time}]);
+          setMessages([...messages, { chat_id, msg, sender, date_time }]);
           scrollToBottom();
         }
       });
     }
   })
-  
+
   useEffect(() => {
     fetchUserData();
   }, [fetchUserData]);
@@ -140,7 +157,7 @@ const UserPage = () => {
     }, 500);
   };
 
-  
+
 
   const thisUser = userData.email;
   console.log('This User:', thisUser);
@@ -171,7 +188,7 @@ const UserPage = () => {
       topMsg: chat.time
     };
   });
-  const handleSend = () =>{
+  const handleSend = () => {
     const chat_id = selectedUser.chat_id;
     const sender = thisUser;
     // const msg = document.querySelector('.input-message').value;
@@ -218,20 +235,20 @@ const UserPage = () => {
               ))}
             </div> */}
             <div ref={chatViewRef} className={`person-chat-view ${isFading ? 'fade-out' : ''}`}>
-  {messages.map((chat, index) => (
-    <ChatBubble key={index} chats={chat} thisUser={thisUser} curr_chat={selectedUser} />
-  ))}
-</div>
+              {messages.map((chat, index) => (
+                <ChatBubble key={index} chats={chat} thisUser={thisUser} curr_chat={selectedUser} />
+              ))}
+            </div>
             <div className="person-chat-input">
               <textarea
                 type="text"
                 placeholder="Type a message"
                 className="input-message"
-                value = {msg}
+                value={msg}
                 onChange={handleChange}
                 required
               />
-              <button className="send-button" onClick = {
+              <button className="send-button" onClick={
                 handleSend
               }>
                 <IoSend />
@@ -252,224 +269,3 @@ const UserPage = () => {
 export default UserPage;
 
 
-
-
-// import React, { useContext, useEffect, useState, useCallback } from "react";
-// import { UserContext } from "../context/UserContext";
-// import { ContactContext } from "../context/ContactContext";
-// import { MessageContext } from "../context/MessageContext";
-// import { useNavigate } from "react-router-dom";
-// import "./UserPage.css";
-// import { IoMdPersonAdd } from "react-icons/io";
-// import { BiLogOut } from "react-icons/bi";
-// import UserCard from "./userCard";
-// import ChatBubble from "./chatBubble";
-// import ChatHeader from "./chatHeader";
-// import AddPersonPopup from "./AddPersonPopup";
-// import Cookies from "js-cookie";
-// import { IoSend } from "react-icons/io5";
-// import io from "socket.io-client";
-
-// const UserPage = () => {
-//   const { userData, fetchUserData } = useContext(UserContext);
-//   const { contacts, fetchContacts } = useContext(ContactContext);
-//   const { messages, fetchMessages } = useContext(MessageContext);
-//   const [msg, setMsg] = useState("");
-//   const navigate = useNavigate();
-//   const [showPopup, setShowPopup] = useState(false);
-//   const [selectedUser, setSelectedUser] = useState(null);
-//   const [isFading, setIsFading] = useState(false);
-//   const [socket, setSocket] = useState(null); // State to hold socket instance
-
-//   const addMsg = useCallback(async (chat_id, sender, msg, date_time) => {
-//     try {
-//       const res = await fetch("http://localhost:3000/user/addmsg/", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//           chat_id: chat_id,
-//           sender: sender,
-//           msg: msg,
-//           date_time: date_time,
-//         }),
-//         credentials: "include",
-//       });
-
-//       const data = await res.json();
-//       if (!res.ok) {
-//         throw new Error(data.detail);
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }, []);
-
-//   // Initialize socket connection
-//   useEffect(() => {
-//     if (userData && userData.email) {
-//       const newSocket = io("http://localhost:3000", {
-//         query: { email: userData.email },
-//       });
-//       setSocket(newSocket);
-
-//       // Clean up socket on component unmount
-//       return () => {
-//         newSocket.disconnect();
-//       };
-//     }
-//   }, [userData]);
-
-//   useEffect(() => {
-//     fetchUserData();
-//   }, [fetchUserData]);
-
-//   useEffect(() => {
-//     if (userData && userData.email) {
-//       fetchContacts(userData.email);
-//     }
-//   }, [userData, fetchContacts]);
-
-//   useEffect(() => {
-//     if (selectedUser) {
-//       fetchMessages(selectedUser.chat_id);
-//     }
-//   }, [selectedUser, fetchMessages]);
-
-//   const handleAddPersonClick = () => {
-//     setShowPopup(true);
-//   };
-
-//   const handleClosePopup = () => {
-//     setShowPopup(false);
-//   };
-
-//   const handleChange = (e) => {
-//     setMsg(e.target.value);
-//   };
-
-//   if (!userData) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (!userData.firstName) {
-//     navigate("/login");
-//     return null; // Ensure no further rendering after redirect
-//   }
-
-//   if (userData.message === "Unauthorized!") {
-//     navigate("/login");
-//     return null; // Ensure no further rendering after redirect
-//   }
-
-//   const handleUserCardClick = (contact) => {
-//     setIsFading(true);
-//     setTimeout(() => {
-//       setSelectedUser(contact);
-//       setIsFading(false);
-//     }, 500);
-//   };
-
-//   const thisUser = userData.email;
-
-//   const handleLogout = async () => {
-//     console.log("Logging out...");
-//     Cookies.remove("token");
-//     try {
-//       const response = await fetch("http://localhost:3000/user/logout", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         credentials: "include",
-//       });
-//       console.log("Logout response:", response);
-//     } catch (error) {
-//       console.error("Error logging out:", error);
-//     }
-//   };
-
-//   const handleSend = () => {
-//     const chat_id = selectedUser.chat_id;
-//     const sender = thisUser;
-//     const msg_user = msg;
-//     setMsg("");
-//     const date_time = new Date().toISOString();
-//     addMsg(chat_id, sender, msg_user, date_time);
-//   };
-
-//   return (
-//     <div className="main-div">
-//       <div className="function-button-column">
-//         <button className="addperson" onClick={handleAddPersonClick}>
-//           <IoMdPersonAdd />
-//         </button>
-//         <button
-//           className="logout"
-//           onClick={() => {
-//             handleLogout();
-//             navigate("/login");
-//           }}
-//         >
-//           <BiLogOut />
-//         </button>
-//       </div>
-
-//       <div className="chatlist-column">
-//         <div className="inner-chatlist-column">
-//           {contacts.map((contact, index) => (
-//             <UserCard
-//               key={index}
-//               index={index}
-//               contact={contact}
-//               onClick={() => handleUserCardClick(contact)}
-//             />
-//           ))}
-//         </div>
-//       </div>
-
-//       <div className="chat-column">
-//         {selectedUser ? (
-//           <>
-//             <ChatHeader
-//               profilePic={selectedUser.profilePic}
-//               name={selectedUser.contact_name}
-//               status={selectedUser.status}
-//             />
-//             <div className={`person-chat-view ${isFading ? "fade-out" : ""}`}>
-//               {messages.map((chat, index) => (
-//                 <ChatBubble
-//                   key={index}
-//                   chats={chat}
-//                   thisUser={thisUser}
-//                   curr_chat={selectedUser}
-//                 />
-//               ))}
-//             </div>
-//             <div className="person-chat-input">
-//               <textarea
-//                 type="text"
-//                 placeholder="Type a message"
-//                 className="input-message"
-//                 value={msg}
-//                 onChange={handleChange}
-//                 required
-//               />
-//               <button className="send-button" onClick={handleSend}>
-//                 <IoSend />
-//               </button>
-//             </div>
-//           </>
-//         ) : (
-//           <div className="welcome-message">
-//             <h1>WELCOME TO X-CHAT</h1>
-//           </div>
-//         )}
-//       </div>
-//       {showPopup && <AddPersonPopup onClose={handleClosePopup} />}
-//     </div>
-//   );
-// };
-
-// export default UserPage;
